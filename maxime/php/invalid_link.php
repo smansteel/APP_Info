8bd8d948dc3e88d8d6b54e4fac5154b1504ec103
sman_steel <59889389+smansteel@users.noreply.github.com>
2022-11-05 01:36:45 +0100

    added roadmap + rangement des pck de db

---
+++ maxime/php/invalid_link.php
@@ -0,0 +1,3 @@
+<?php
+// invalid confirmation link with token (expired/ not in db) réessayer la procédure ou ,ous contacter a contact@captair.paris
+?>
---
+++ maxime/php/resetpassword.php
@@ -0,0 +1,3 @@
+<?php
+// add method to collect passwd and confirmation and send the form (POST) to changepasswd.php
+?>
