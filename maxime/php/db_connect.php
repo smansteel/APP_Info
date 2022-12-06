9764fe56dc3e78363f6d9563dd7a0a43bf12af37
sman_steel <59889389+smansteel@users.noreply.github.com>
2022-12-06 10:32:47 +0100

    get env password

--- maxime/php/db_connect.php
+++ maxime/php/db_connect.php
@@ -3,7 +3,7 @@ function OpenCon()
 {
     $dbhost = "localhost:3306";
     $dbuser = "root";
-    $dbpass = "test";
+    $dbpass = getenv("DB_PASSWORD");
     $db = "captair";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
