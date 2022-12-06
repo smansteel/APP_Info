4d8fba813bdc4310505fc2c4210a3a0ce86cb399
sman_steel <59889389+smansteel@users.noreply.github.com>
2022-12-06 11:45:43 +0100

    fix db

--- maxime/php/db_connect.php
+++ maxime/php/db_connect.php
@@ -4,12 +4,12 @@ function OpenCon()
     $dbhost = "localhost:3306";
 
     // windows
-    // $dbuser = getenv('DB_USERNAME');
-    // $dbpass =  getenv("DB_PASSWORD");
+    $dbuser = getenv('DB_USERNAME');
+    $dbpass =  getenv("DB_PASSWORD");
 
     //server
-    $dbuser = $_ENV["DB_USERNAME"];
-    $dbpass =  $_ENV["DB_PASSWORD"];
+    // $dbuser = $_ENV["DB_USERNAME"];
+    // $dbpass =  $_ENV["DB_PASSWORD"];
     $db = "captair";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
