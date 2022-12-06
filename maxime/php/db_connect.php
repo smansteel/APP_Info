b2393fa88b1ee884ab901770c655e77a571dc6f4
sman_steel <59889389+smansteel@users.noreply.github.com>
2022-12-06 11:25:17 +0100

    undo

--- maxime/php/db_connect.php
+++ maxime/php/db_connect.php
@@ -2,8 +2,8 @@
 function OpenCon()
 {
     $dbhost = "localhost:3306";
-    $dbuser = $_ENV["DB_USERNAME"];
-    $dbpass =  $_ENV["DB_PASSWORD"];
+    $dbuser = getenv('DB_USERNAME');
+    $dbpass =  getenv("DB_PASSWORD");
     $db = "captair";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
