6979b05794733b49b6f4a885dd2684c64de20fcc
sman_steel <59889389+smansteel@users.noreply.github.com>
2022-12-13 11:13:41 +0100

    dynamic header & footer

--- maxime/php/index.php
+++ maxime/php/index.php
@@ -16,24 +16,7 @@
 
 
 <body>
-    <header>
-        <nav>
-            <div class="logo">
-                <img src="images/air_Q_full_turquoise_v3.svg">
-            </div>
-            <div class="toggle">
-                <i class="fa-solid fa-bars ouvrir"></i>
-                <i class="fa-solid fa-xmark fermer"></i>
-            </div>
-            <ul class="menu">
-                <li><a href="#">A Propos</a></li>
-                <li><a href="#">Données</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><button class="btn">Connexion</button></li>
-            </ul>
-        </nav>
-    </header>
-
+<?php include("header.php")?>
 
     <div class="bodyparagraph">
 
@@ -200,29 +183,6 @@
 
 
 
-    <footer>
-
-        <div class=" contenu-footer">
-            <div class="bloc footer-services">
-                <h4>&copy; 2022 AirQ</h4>
-            </div>
-
-            <div class="liens">
-                <div class="bloc footer-services">
-                    <a href="#">Nous contacter</a>
-                </div>
-                <div class="bloc footer-services">
-                    <a href="#">Mentions légales</a>
-                </div>
-                <div class="bloc footer-services">
-                    <a href="#">CGU</a>
-                </div>
-                <div class="bloc footer-services">
-                    <a href="#">Politique de confidentialité</a>
-                </div>
-            </div>
-
-        </div>
-    </footer>
+    <?php include("footer.php")?>
     </div>
 </body>
