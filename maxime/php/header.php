ab8dda6c042680f2c553cbb841002ac001486a81
Alexandre Pérou <64700694+nigelnokushimodo@users.noreply.github.com>
2022-12-15 19:51:27 +0100

    faq temporaire

--- maxime/php/header.php
+++ maxime/php/header.php
@@ -1,24 +1,22 @@
 <link rel="stylesheet" href="header_footer.css">
- <!-- Font Awesome logos -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- Font Awesome logos -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
-    <header>
-        <nav> <a href="/">
-               <img class="logo-header" src="sources/air_Q_full_turquoise_v3.svg">
-            </a>
+<header>
+    <nav> <a href="/">
+            <img class="logo-header" src="sources/air_Q_full_turquoise_v3.svg">
+        </a>
 
-            <ul class="menu">
-                <li><a href="/">A Propos</a></li>
-                <li><a href="/donnees.php">Données</a></li>
-                <li><a href="/faq.php">FAQ</a></li>
-                <li><button class="btn"><a href="/login.php">Connexion</a></button></li>
-            </ul>
-                        <div class="toggle">
-                <i class="fa-solid fa-bars ouvrir"></i>
-                <i class="fa-solid fa-xmark fermer"></i>
-            </div>
-        </nav>
-    </header>
+        <ul class="menu">
+            <li><a href="/">A Propos</a></li>
+            <li><a href="/donnees.php">Données</a></li>
+            <li><a href="/faq tempo.php">FAQ</a></li>
+            <li><button class="btn"><a href="/login.php">Connexion</a></button></li>
+        </ul>
+        <div class="toggle">
+            <i class="fa-solid fa-bars ouvrir"></i>
+            <i class="fa-solid fa-xmark fermer"></i>
+        </div>
+    </nav>
+</header>
