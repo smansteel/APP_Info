9210f6bc797c8e6a258cc9d36f3012eb4b2f9094
sman_steel <59889389+smansteel@users.noreply.github.com>
2022-12-16 10:48:55 +0100

    Update verified.php

--- maxime/php/verified.php
+++ maxime/php/verified.php
@@ -3,23 +3,24 @@
 <link rel="stylesheet" href="login.css">
 
 <body>
-    <div class="box">
-        <div class="flex-content">
+
+    <div class="fb-page">
             <?php include("header.php"); ?>
+
+        <div class="box">
+
+            <div class="bg-image">
+                <img src="sources/captair.png" class="logo">
+                <div class="text">
+                    La vérification d'adresse mail a fonctionné.<br> Nous vous invitons à rejoindre la page mon compte pour accéder a vos données.
+                </div>
+                <br>
+                <a class="newacc" href="/login.php" class="newacc">Se Connecter</a><br>
+            </div>
+
+                <?php include("footer.php"); ?>
         </div>
 
-        <div class="bwate"></div>
-        <div class="bg-image">
-            <img src="sources/captair.png" class="logo">
-            <div class="text">
-                La vérification d'adresse mail a fonctionné.<br> Nous vous invitons à rejoindre la page mon compte pour accéder a vos données.
-            </div>
-            <br>
-            <a class="newacc" href="/login.php" class="newacc">Se Connecter</a><br>
-        </div>
-        <div class="flex-content" id="footer">
-            <?php include("footer.php"); ?>
-        </div>
 
     </div>
 </body>
