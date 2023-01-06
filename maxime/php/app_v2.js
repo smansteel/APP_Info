0e292bf3f9f0ed6f12a8f9c0b1d3912296b1f320
Alexandre Pérou <64700694+nigelnokushimodo@users.noreply.github.com>
2023-01-06 14:38:44 +0100

    stop scroll

--- maxime/php/app_v2.js
+++ maxime/php/app_v2.js
@@ -64,20 +64,11 @@ document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
 })
 
 
-window.onload = function () {
-    window.addEventListener('scroll', function (e) {
-        if (window.pageYOffset > 100) {
-            document.querySelector("header").classList.add('is-scrolling');
-        } else {
-            document.querySelector("header").classList.remove('is-scrolling');
-        }
-    });
+const menu_btn = document.querySelector('.toggle');
+const mobile_menu = document.querySelector('.mobile-nav');
 
-    const menu_btn = document.querySelector('.toggle');
-    const mobile_menu = document.querySelector('.mobile-nav');
-
-    menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle('is-active');
-        mobile_menu.classList.toggle('is-active');
-    });
-}
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    document.body.style.overflow = mobile_menu.classList.contains('is-active') ? 'hidden' : 'auto';
+});
