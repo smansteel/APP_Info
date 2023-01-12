9cb7e75c29d831af6e92b0ac65d17f604d4b4537
sman_steel <59889389+smansteel@users.noreply.github.com>
2023-01-12 11:32:10 +0100

    avancement mvc

--- maxime/mvc_test/app/controllers/home.php
+++ maxime/mvc_test/app/controllers/home.php
@@ -2,8 +2,11 @@
 
 class Home extends Controller
 {
-    public function index()
+    public function index($name = '')
     {
-        echo 'home/index';
+        $user = $this->model('User');
+        $user->name = $name;
+
+        $this->view('home/index');
     }
 }
--- maxime/mvc_test/app/core/Controller.php
+++ maxime/mvc_test/app/core/Controller.php
@@ -4,6 +4,13 @@ class Controller
 {
     public function model($model)
     {
-        echo $model;
+        require_once '../app/models/' . $model . '.php';
+        return new $model();
+    }
+
+        public function view($view, $data = [])
+    {
+        require_once '../app/views/' . $view . '.php';
     }
 }
+
