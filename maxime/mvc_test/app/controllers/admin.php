b384f5149399a5aeb2987b108c0a0c43e85ad40e
sman_steel <59889389+smansteel@users.noreply.github.com>
2023-01-16 16:14:57 +0100

    admin updates

--- maxime/mvc_test/app/controllers/admin.php
+++ maxime/mvc_test/app/controllers/admin.php
@@ -3,8 +3,31 @@ class Admin extends Controller
 {
     public function index()
     {
-        $this->view('header_footer/header');
-        $this->view('admin/index');
-        $this->view('header_footer/footer');
+       $this->users();
+    }
+
+    public function users(){
+        $this->model('Database');
+        $db = new Database;
+        $selected_fields = ["id", "email", "nom", "prenom", "creation", "verified"];
+        $table = "users";
+        $where_column = "verified";
+        $where_value = "1";
+        $db->select($selected_fields, $table, $where_column, $where_value);
+        $userlist = $db->return_list();
+        $db->close();
+        $db = new Database;
+        $selected_fields = ["id", "email", "nom", "prenom", "creation", "verified"];
+        $table = "capteurs";
+        $where_column = "verified";
+        $where_value = "1";
+        $db->select($selected_fields, $table, $where_column, $where_value);
+        $userlist = $db->return_list();
+        $db->close();
+
+
+
+        $this->view('admin/users', ['userlist' => $userlist]);
+
     }
 }
