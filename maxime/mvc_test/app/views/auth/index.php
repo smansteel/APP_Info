85767d97b910f2a89d640d6feb595a12a1ba8f33
henri_mariolle <114931996+Henri-Mariolle@users.noreply.github.com>
2023-01-20 13:46:46 +0100

    updated index auth

--- maxime/mvc_test/app/views/auth/index.php
+++ maxime/mvc_test/app/views/auth/index.php
@@ -53,8 +53,7 @@
                     </div>
 
                     <form action="<?= $root ?>/auth/login/" method="post" class="form">
-
-                        <<<<<<< Updated upstream <div class="form-connect">
+                    <div class="form-connect">
                             <input type="email" name="email" id="email" class="form_field" placeholder="Adresse email" required>
                 </div>
                 <div class="form-example">
@@ -68,22 +67,7 @@
                 <a class="newacc" href="<?= $root ?>/login/inscription" class="newacc">Créer mon compte</a><br>
                 <br>
                 <a href="<?= $root ?>/login/forgor" class="fgtpsswd">Mot de passe oublié</a>
-                =======
-                <div class="form-connect">
-                    <input type="email" name="email" id="email" class="form_field" placeholder="Adresse email" required>
-                </div>
-                <div class="form-example">
-                    <input type="password" name="password" id="password" class="form_field" placeholder="Mot de passe" required>
-                </div>
-                <div class="form-example">
-                    <input type="submit" value="Connexion" class="submit_button">
-                </div>
-                </form>
-                <br>
-                <a class="newacc" href="<?= $root ?>/login/inscription" class="newacc">Créer mon compte</a><br>
-                <br>
-                <a href="/resetpassword.php" class="fgtpsswd">Mot de passe oublié</a>
-                >>>>>>> Stashed changes
+                
 
 
             </div>
