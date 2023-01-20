f55acfbfacb2cf47c9c35e146ad6d39a9cbf8d01
henri_mariolle <114931996+Henri-Mariolle@users.noreply.github.com>
2023-01-20 14:23:08 +0100

    update js

--- maxime/mvc_test/app/views/home/index.php
+++ maxime/mvc_test/app/views/home/index.php
@@ -18,12 +18,13 @@ $root = "/public";
     <meta name="viewport" content="with=device-wdith, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= $css ?>home.css">
-    <link rel="stylesheet" href="<?= $js ?>app_v2.js">
+
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <title>Accueil - AirQ</title>
     <script src="<?= $js ?>ajax_form.js"></script>
+        <script src="<?= $js ?>app_v2.js"></script>
 </head>
 
 <body>
